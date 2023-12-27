7c7df4948f3314bbbfec1b8040c5b69e907980ce
Iconians <claytoncripe@gmail.com>
2023-12-26 19:55:09 -0800

    got supabase setup

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare interface ImportMetaEnv {
+  VITE_SUPABASE_URL?: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  env: ImportMetaEnv
+}
