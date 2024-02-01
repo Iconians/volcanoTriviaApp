ba05d3c0b8f97d37eb1254a3e939db2404c2bffe
Iconians <claytoncripe@gmail.com>
2024-02-01 14:57:04 -0800

    got high score page done working on minor fixes

--- src/pages/hiScores.vue
+++ src/pages/hiScores.vue
@@ -19,23 +19,45 @@ fetchScores();
 </script>
 
 <template>
-  <section class="h-full w-full text-center bg-stHelensWithPlume bg-cover">
-    <div class="flex flex-col justify-around h-full">
+  <section class="highScoreSection h-full w-full text-center bg-stHelensWithPlume bg-cover text-white">
+    <div class="pageWrapper flex flex-col justify-around h-full">
     <div class="">
-      <h1>High scores</h1>
+      <h1>High Scores</h1>
     </div>
     <!-- finish designing this portion of the site -->
-    <div class="overflow-y-auto ">
+    <div class="overflow-y-auto h-52">
     <div class="flex m-5 justify-center" v-for="score in scores" :key="score.score">
-      <div>{{score.user_name}}</div>
+      <div class="mr-3">{{score.user_name}}</div>
       <div>{{score.score}}</div>
     </div>
     </div>
     <div>
-      <router-link to="/">Back to Home</router-link>
+      <router-link class="mr-8" to="/">Back to Home</router-link>
       <router-link to="/userPage">Profile</router-link>
     </div>
   </div>
   </section>
 
 </template>
+
+<style scoped>
+.highScoreSection {
+  position: relative;
+}
+  .highScoreSection::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: black;
+  opacity: 0.4;
+  z-index: 1;
+}
+.pageWrapper {
+  position: relative;
+  z-index: 2;
+}
+
+</style>
