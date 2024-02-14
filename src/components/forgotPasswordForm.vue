b76581e6de66886cce3b3c2efea717650abf2648
Iconians <claytoncripe@gmail.com>
2024-02-13 17:52:11 -0800

    put the api info in the components for the forgot password form

---
+++ src/components/forgotPasswordForm.vue
@@ -0,0 +1,63 @@
+<!-- Update User
+Update the user with a new email or password. Each key (email, password, and data) is optional
+
+UPDATE USER
+const { data, error } = await supabase.auth.updateUser({
+  email: "new@email.com",
+  password: "new-password",
+  data: { hello: 'world' }
+}) -->
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { supabase } from '../../supabase.js'
+export default defineComponent({
+  name: 'forgotPasswordForm',
+  data() {
+    return {
+      formError: ''
+    }
+  },
+  methods: {
+    async resetPassword(event: Event) {
+      event.preventDefault()
+
+      const form = event.target as HTMLFormElement
+      const email = form.email.value
+      const password = form.password.value
+
+      // try {
+      //   const { data, error } = await supabase.auth.signInWithPassword({
+      //     email,
+      //     password,
+      //   })
+
+      //   if (error) {
+      //     this.formError = error.message
+      //   } else {
+      //     this.$emit('signedIn', data.user)
+      //   }
+      // } catch (error) {
+      //   console.error('Error in signIn method: ', error)
+      // }
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="text-wrapper text-white">
+    <h1 class="text-2xl">Volcano Quiz</h1>
+    <p>Please sign in to see if you are smart enough to be a Volcanologist?</p>
+    <div>
+      <form @submit="resetPassword" class="flex flex-col">
+        <label class="text-xl" for="username">Email</label>
+        <input class="rounded-sm text-black" type="email" id="username" name="email" />
+        <label class="mt-2 text-xl" for="password">password</label>
+        <input class="rounded text-black" type="password" id="password" name="password" />
+        <input class="rounded text-xl mt-3 bg-brown-500" type="submit" value="submit" />
+      </form>
+      <div v-if="formError.length">{{ formError }}</div>
+    </div>
+  </div>
+</template>
--- src/pages/startPage.vue
+++ src/pages/startPage.vue
@@ -1,7 +1,14 @@
+<!-- Forgotten Password Email
+Sends the user a log in link via email. Once logged in you should direct the user to a new password form. And use "Update User" below to save the new password.
+
+PASSWORD RECOVERY
+let { data, error } = await supabase.auth.resetPasswordForEmail(email) -->
+
 <script setup lang="ts">
 import startComponent from '@/components/startComponent.vue'
 import signInForm from '@/components/signInForm.vue'
 import createAcctForm from '@/components/createAcctForm.vue'
+import forgotPasswordForm from '@/components/forgotPasswordForm.vue'
 import { ref } from 'vue'
 
 interface User {
@@ -9,15 +16,15 @@ interface User {
   displayName: string
 }
 
-const isCreatingAccount = ref(false);
-const isSignedIn = ref(false);
+const isCreatingAccount = ref(false)
+const isSignedIn = ref(false)
+const forgotPassword = ref(false)
 
 const alreadySingedIn = () => {
   const user = localStorage.getItem('user')
   if (user !== null) {
     isSignedIn.value = true
-  }
-  else {
+  } else {
     isSignedIn.value = false
   }
 }
@@ -31,19 +38,18 @@ const signIn = (user: any) => {
   localStorage.setItem('user', JSON.stringify(user))
 }
 alreadySingedIn()
-
 </script>
 
 <template>
-  <section class="section bg-volcanoGif height h-full w-full text-center"  >
+  <section class="section bg-volcanoGif height h-full w-full text-center">
     <sign-in-form v-if="!isCreatingAccount && !isSignedIn" @signedIn="signIn" />
-    <create-acct-form v-if="isCreatingAccount" @accountCreated="handleAccountCreated"/>
+    <create-acct-form v-if="isCreatingAccount" @accountCreated="handleAccountCreated" />
     <start-component v-if="isSignedIn" />
+    <forgot-password-form v-if="forgotPassword" />
     <div>
       <button v-if="!isSignedIn" @click="isCreatingAccount = !isCreatingAccount" class="text-white">
         {{ isCreatingAccount ? 'Sign In' : 'Create Account' }}
       </button>
     </div>
   </section>
-
 </template>
