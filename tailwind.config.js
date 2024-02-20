60f8b929a5abbac0c6fb325036cdd9a2c3dcdcca
Iconians <claytoncripe@gmail.com>
2024-02-20 11:54:01 -0800

    moved images to the puublic folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        volcanoGif: "url('/src/assets/vocanicEuruption1.gif')",
-        stHelensWithtop: "url('src/assets/sthelens1-animation-eb3a7d-640.gif')",
-        stHelensWithPlume: "url('src/assets/st-helens.webp')",
-        lavaVolcano: "url('src/assets/lava-volcano-image.jpg')",
-        activeVolcano: "url('src/assets/active-volcano-turrialba-in-costa-rica.jpg')",
+        volcanoGif: "url('/vocanicEuruption1.gif')",
+        stHelensWithtop: "url('/sthelens1-animation-eb3a7d-640.gif')",
+        stHelensWithPlume: "url('/st-helens.webp')",
+        lavaVolcano: "url('/lava-volcano-image.jpg')",
+        activeVolcano: "url('/active-volcano-turrialba-in-costa-rica.jpg')",
       },
       colors: {
         brown: {
