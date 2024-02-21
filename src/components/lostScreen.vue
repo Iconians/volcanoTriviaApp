c90fae255ab9933f5361f7457b56730a06d43ba1
Iconians <claytoncripe@gmail.com>
2024-02-20 17:04:08 -0800

    made some space between words on lostScreen

--- src/components/lostScreen.vue
+++ src/components/lostScreen.vue
@@ -14,13 +14,13 @@ const props = defineProps({
       <div class="text-center text-2xl mb-7">
         <p>would you be this close to an active volcano?</p>
       </div>
-      <div class="text-2xl text-center">
-        <p>Correct {{ props.correctAnswers }}</p>
-        <p>Incorrect {{ props.wrongAnswers }}</p>
+      <div class="text-2xl text-center z-10">
+        <p class="m-2">Correct {{ props.correctAnswers }}</p>
+        <p class="m-2">Incorrect {{ props.wrongAnswers }}</p>
       </div>
       <div class="flex flex-col justify-center align-center">
-        <router-link to="/totals" class="text-center text-2xl">High Scores</router-link>
-        <router-link to="/" class="text-center text-2xl">Return Home</router-link>
+        <router-link to="/totals" class="text-center text-2xl m-2">High Scores</router-link>
+        <router-link to="/" class="text-center text-2xl m-2">Return Home</router-link>
       </div>
     </div>
   </section>
