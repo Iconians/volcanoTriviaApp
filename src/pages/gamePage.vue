161e4ff4d6e14fbde8955c9eeda44851303234aa
Iconians <claytoncripe@gmail.com>
2024-02-22 14:29:51 -0800

    redid a couple functions so they are more readable on the gamepage

--- src/pages/gamePage.vue
+++ src/pages/gamePage.vue
@@ -24,72 +24,104 @@ const answerArray = ref<answerArr[]>([])
 const wrongAnswers = ref(0)
 const correctAnswers = ref(0)
 
+const findUser = () => {
+  const user = localStorage.getItem('user')
+  let userId
+  if (user !== null) {
+    userId = JSON.parse(user).id
+    return userId
+  } else {
+    console.error('No user found')
+  }
+}
+
+const findScore = async (userId: string | null) => {
+  const { data: currentScore, error: fetchError } = await supabase
+    .from('profile')
+    .select('score')
+    .eq('user_id', userId)
+  if (fetchError) {
+    console.error('Error fetching score: ', fetchError)
+    return
+  }
+  return currentScore
+}
+
+const addScore = async (currentScore: any | undefined, userId: any) => {
+  const newScore = { correct: correctAnswers.value, incorrect: wrongAnswers.value }
+  currentScore[0].score.push(newScore)
+
+  const { data, error: updateError } = await supabase
+    .from('profile')
+    .update({ score: currentScore[0].score })
+    .match({ user_id: userId })
+  console.log('currentScore', updateError, data)
+  if (updateError) {
+    console.error('Error posting score: ', updateError)
+  }
+}
+
+const findDisplayName = async (userId: any) => {
+  const { data: currentScore, error: fetchError } = await supabase
+    .from('profile')
+    .select('display_name')
+    .eq('user_id', userId)
+
+  if (fetchError) {
+    console.error('Error fetching score: ', fetchError)
+    return
+  }
+  return currentScore
+}
+
+const updateSupabase = async (displayName: any) => {
+  const { error: highScoreError } = await supabase.from('high_score').select('*')
+
+  if (highScoreError) {
+    console.error('Error fetching high score: ', highScoreError)
+    return
+  }
+  const { error: updateHighScoreError } = await supabase
+    .from('high_score')
+    .insert({ score: correctAnswers.value, user_name: displayName })
+
+  if (updateHighScoreError) {
+    console.error('Error posting high score: ', updateHighScoreError)
+  }
+}
+
 async function postScore() {
   if (questionsArray.value.length === 0 || wrongAnswers.value === 3) {
-    const user = localStorage.getItem('user')
-    let userId
-    if (user !== null) {
-      userId = JSON.parse(user).id
-    } else {
-      console.error('No user found')
-    }
-
-    const { data: currentScore, error: fetchError } = await supabase
-      .from('profile')
-      .select('score')
-      .eq('user_id', userId)
-    if (fetchError) {
-      console.error('Error fetching score: ', fetchError)
-      return
-    }
-
-    const newScore = { correct: correctAnswers.value, incorrect: wrongAnswers.value }
-    currentScore[0].score.push(newScore)
-
-    const { data, error: updateError } = await supabase
-      .from('profile')
-      .update({ score: currentScore[0].score })
-      .match({ user_id: userId })
-    console.log('currentScore', updateError, data)
-    if (updateError) {
-      console.error('Error posting score: ', updateError)
-    }
+    const userId = findUser()
+    const currentScore = await findScore(userId)
+    addScore(currentScore, userId)
   }
   if (correctAnswers.value >= 4) {
-    const user = localStorage.getItem('user')
-    let userId
-    if (user !== null) {
-      userId = JSON.parse(user).id
-    } else {
-      console.error('No user found')
-    }
-
-    const { data: currentScore, error: fetchError } = await supabase
-      .from('profile')
-      .select('display_name')
-      .eq('user_id', userId)
-
-    if (fetchError) {
-      console.error('Error fetching score: ', fetchError)
-      return
-    }
-
-    const displayName = currentScore[0].display_name
-
-    const { error: highScoreError } = await supabase.from('high_score').select('*')
-
-    if (highScoreError) {
-      console.error('Error fetching high score: ', highScoreError)
-      return
-    }
-    const { error: updateHighScoreError } = await supabase
-      .from('high_score')
-      .insert({ score: correctAnswers.value, user_name: displayName })
-
-    if (updateHighScoreError) {
-      console.error('Error posting high score: ', updateHighScoreError)
+    const userId = findUser()
+    const currentScore = await findDisplayName(userId)
+    if (currentScore !== undefined) {
+      const displayName = currentScore[0].display_name
+      updateSupabase(displayName)
     }
   }
+}
+
+const findAnswer = (correctAnswer: answerArr[], answer: string) => {
+  const getAnswer = correctAnswer[0].correct_answer
+  if (answer.toString() === getAnswer) {
+    correctAnswers.value++
+  } else {
+    wrongAnswers.value++
+  }
+}
+
+const sortQuestions = () => {
+  questionsArray.value.shift()
+  answerArray.value.forEach((a) => {
+    if (questionsArray.value.length > 0 && a.question_foreign_key === questionsArray.value[0].id) {
+      a.answers.sort(() => Math.random() - 0.5)
+    }
+  })
 }
 
 const checkAnswer = async (answer: string) => {
@@ -98,12 +130,7 @@ const checkAnswer = async (answer: string) => {
   )
 
   if (correctAnswer.length > 0) {
-    const getAnswer = correctAnswer[0].correct_answer
-    if (answer.toString() === getAnswer) {
-      correctAnswers.value++
-    } else {
-      wrongAnswers.value++
-    }
+    findAnswer(correctAnswer, answer)
   }
 
   if (wrongAnswers.value === 3) {
@@ -111,15 +138,7 @@ const checkAnswer = async (answer: string) => {
   }
 
   if (questionsArray.value.length > 0) {
-    questionsArray.value.shift()
-    answerArray.value.forEach((a) => {
-      if (
-        questionsArray.value.length > 0 &&
-        a.question_foreign_key === questionsArray.value[0].id
-      ) {
-        a.answers.sort(() => Math.random() - 0.5)
-      }
-    })
+    sortQuestions()
   } else {
     console.log('No more questions')
   }
