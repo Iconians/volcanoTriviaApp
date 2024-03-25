174bca2ac49df364f0688bbb7b76e136bd76f205
Iconians <claytoncripe@gmail.com>
2024-03-24 17:55:47 -0700

    made a warning modal skeleton need to finish styling it

--- src/components/startComponent.vue
+++ src/components/startComponent.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
-import { defineEmits } from 'vue'
+import { defineEmits, ref } from 'vue'
+import warningModal from '@/components/warningModal.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faVolumeHigh, faVolumeXmark } from '@fortawesome/free-solid-svg-icons'
 
 const emit = defineEmits(['click'])
+const isModalOpen = ref(false)
+
+const openModal = () => {
+  console.log(isModalOpen.value)
+  isModalOpen.value = true
+  console.log(isModalOpen.value)
+}
+
+const closeModal = () => {
+  console.log(isModalOpen.value)
+  isModalOpen.value = false
+  console.log(isModalOpen.value)
+}
 
 const listenForAudioClick = () => {
   emit('click')
@@ -13,16 +27,16 @@ const listenForAudioClick = () => {
 const props = defineProps({
   playing: Boolean
 })
-console.log(props.playing)
 const isPlaying = () => props.playing && props.playing === true
 </script>
 
 <template>
-  <div class="text-wrapper text-white bg-black/50 p-12 rounded-3xl">
+  <div class="text-wrapper text-white bg-black/50 p-12 rounded-3xl relative">
     <div>
       <h1 class="text-3xl">Volcano Quiz</h1>
       <p class="text-2xl font-thin">Are you smart enough to be a volcanologist?</p>
     </div>
+    <warningModal :isModalOpen="isModalOpen" :closeModal="closeModal" />
     <RouterLink class="text-2xl text-center m-2" to="/gamePage">Normal Game</RouterLink>
     <RouterLink class="text-2xl text-center m-2" to="/godModeGamePage">Master Game</RouterLink>
     <RouterLink class="text-2xl text-center m-2" to="/userPage">Profile</RouterLink>
@@ -39,5 +53,6 @@ const isPlaying = () => props.playing && props.playing === true
       class="text-white cursor-pointer"
       @click="listenForAudioClick"
     />
+    <button @click="openModal" class="">WARNING</button>
   </div>
 </template>
---
+++ src/components/warningModal.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
+const props = defineProps({
+  isModalOpen: Boolean,
+  closeModal: Function
+})
+
+const emit = defineEmits(['click'])
+
+const onClick = () => {
+  props.closeModal?.()
+  emit('click')
+}
+
+console.log(props.isModalOpen)
+</script>
+
+<template>
+  <section class="parent">
+    <div :class="{ isModalOpen: props.isModalOpen, modal: true }">
+      <div class="modal-content">
+        <h4>Warning</h4>
+        <FontAwesomeIcon :icon="faTimes" @click="onClick" />
+      </div>
+      <div>
+        <p>Play the Master Game at your own risk</p>
+        <p>
+          there are 100 questions to answer so if you think you can prove your as smart as a
+          Volcanologist give it a shot
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.parent .isModalOpen {
+  display: block;
+}
+
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+}
+.modal-content {
+  /* background-color: #fefefe; */
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+</style>
